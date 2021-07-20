017d1fd19137adfd32758ba76c774c4402e3259e
Fatima <fcamero12@gmail.com>
2021-07-20 10:59:17 -0700

    Completed phonebook backend step 3

--- index.js
+++ index.js
@@ -27,8 +27,19 @@ persons = [
 ]
 
 app.get('/api/persons', (request, response) => {
-    console.log('getting all persons')
-    return response.json(persons)
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+    
 })
 
 app.get('/info', (request, response) => {
